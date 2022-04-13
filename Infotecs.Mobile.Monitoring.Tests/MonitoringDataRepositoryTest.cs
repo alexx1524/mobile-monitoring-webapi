4c09afeadb821b9014653f688881aa9fcec52878
Bolelov Aleksei <aleksei.bolelov@redlab.dev>
2022-04-13 09:12:16 +0300

    Добавлены дополнительные данные для теста

--- Infotecs.Mobile.Monitoring.Tests/MonitoringDataRepositoryTest.cs
+++ Infotecs.Mobile.Monitoring.Tests/MonitoringDataRepositoryTest.cs
@@ -11,154 +11,150 @@ using Moq;
 using Npgsql;
 using Xunit;
 
-namespace Infotecs.Mobile.Monitoring.Tests
+namespace Infotecs.Mobile.Monitoring.Tests;
+
+/// <summary>
+/// Тест для репозитория мониторинговых данных.
+/// </summary>
+public class MonitoringDataRepositoryTest
 {
+    private readonly IConfiguration config;
+
     /// <summary>
-    /// Тест для репозитория мониторинговых данных.
+    /// Конструктор.
     /// </summary>
-    public class MonitoringDataRepositoryTest
+    public MonitoringDataRepositoryTest()
     {
-        private readonly IConfiguration config;
+        this.config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+    }
 
-        /// <summary>
-        /// Конструктор.
-        /// </summary>
-        public MonitoringDataRepositoryTest()
+    /// <summary>
+    /// Метод проверки возврата исключения при невалидных данных.
+    /// </summary>
+    /// <param name="monitoringData">Данные мониторинга.</param>
+    /// <returns>Задача.</returns>
+    [Theory]
+    [ClassData(typeof(WrongMonitoringData))]
+    public async Task MonitoringData_Incorrect_data_throws_exception(MonitoringData monitoringData)
+    {
+        await ClearData();
+
+        // Arrange
+        var context = new DapperContext(config);
+        var loggerMock = new Mock<ILogger<MonitoringDataRepository>>();
+
+        var repository = new MonitoringDataRepository(context, loggerMock.Object);
+
+        // Assert
+        await Assert.ThrowsAsync<PostgresException>(async() => await repository.CreateAsync(monitoringData));
+    }
+
+    /// <summary>
+    /// Проверка успешного создания мониторинговых данных.
+    /// </summary>
+    /// <returns>Задача.</returns>
+    [Fact]
+    public async Task MonitoringData_Success_adding()
+    {
+        await ClearData();
+
+        // Arrange
+        var context = new DapperContext(config);
+        var loggerMock = new Mock<ILogger<MonitoringDataRepository>>();
+
+        var repository = new MonitoringDataRepository(context, loggerMock.Object);
+
+        var data = new MonitoringData
         {
-            this.config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-        }
+            Id = Guid.NewGuid().ToString(),
+            Version = "1.2.3",
+            NodeName = "Name",
+            OperatingSystem = "Android",
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+        };
 
-        /// <summary>
-        /// Метод проверки возврата исключения при невалидных данных.
-        /// </summary>
-        /// <returns>Задача.</returns>
-        [Fact]
-        public async Task MonitoringData_Incorrect_data_throws_exception()
+        // Act
+        await repository.CreateAsync(data);
+
+        MonitoringData? result = await repository.GetByIdAsync(data.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(result?.Id);
+        Assert.NotNull(result?.NodeName);
+        Assert.NotNull(result?.Version);
+        Assert.NotNull(result?.OperatingSystem);
+        Assert.NotNull(result?.CreatedDate);
+        Assert.NotNull(result?.UpdatedDate);
+        Assert.Equal(data.Id, result?.Id);
+        Assert.Equal(data.Version, result?.Version);
+        Assert.Equal(data.NodeName, result?.NodeName);
+        Assert.Equal(data.OperatingSystem, result?.OperatingSystem);
+        Assert.Equal(data.CreatedDate?.ToString("u"), result?.CreatedDate?.ToString("u"));
+        Assert.Equal(data.UpdatedDate?.ToString("u"), result?.UpdatedDate?.ToString("u"));
+    }
+
+    /// <summary>
+    /// Проверка успешного обновления мониторинговых данных.
+    /// </summary>
+    /// <returns>Задача.</returns>
+    [Fact]
+    public async Task MonitoringData_Success_updating()
+    {
+        await ClearData();
+
+        // Arrange
+        var context = new DapperContext(config);
+        var loggerMock = new Mock<ILogger<MonitoringDataRepository>>();
+
+        var repository = new MonitoringDataRepository(context, loggerMock.Object);
+
+        var data = new MonitoringData
         {
-            await ClearData();
+            Id = Guid.NewGuid().ToString(),
+            Version = "1.2.3",
+            NodeName = "Name",
+            OperatingSystem = "Android",
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+        };
 
-            // Arrange
-            var context = new DapperContext(config);
-            var loggerMock = new Mock<ILogger<MonitoringDataRepository>>();
+        // Act
+        await repository.CreateAsync(data);
 
-            var repository = new MonitoringDataRepository(context, loggerMock.Object);
+        data.NodeName = "Node name";
+        data.Version = "1.1.1";
+        data.OperatingSystem = "Ios";
+        data.CreatedDate = DateTime.UtcNow;
+        data.UpdatedDate = DateTime.UtcNow;
 
-            var data = new MonitoringData
-            {
-                Id = null,
-            };
+        await repository.UpdateAsync(data);
 
-            // Assert
-            await Assert.ThrowsAsync<PostgresException>(async() => await repository.CreateAsync(data));
-        }
+        MonitoringData? result = await repository.GetByIdAsync(data.Id);
 
-        /// <summary>
-        /// Проверка успешного создания мониторинговых данных.
-        /// </summary>
-        /// <returns>Задача.</returns>
-        [Fact]
-        public async Task MonitoringData_Success_adding()
-        {
-            await ClearData();
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotNull(result?.Id);
+        Assert.NotNull(result?.NodeName);
+        Assert.NotNull(result?.Version);
+        Assert.NotNull(result?.OperatingSystem);
+        Assert.NotNull(result?.CreatedDate);
+        Assert.NotNull(result?.UpdatedDate);
+        Assert.Equal(data.Id, result?.Id);
+        Assert.Equal(data.NodeName, result?.NodeName);
+        Assert.Equal(data.Version, result?.Version);
+        Assert.Equal(data.OperatingSystem, result?.OperatingSystem);
+        Assert.Equal(data.CreatedDate?.ToString("u"), result?.CreatedDate?.ToString("u"));
+        Assert.Equal(data.UpdatedDate?.ToString("u"), result?.UpdatedDate?.ToString("u"));
+    }
 
-            // Arrange
-            var context = new DapperContext(config);
-            var loggerMock = new Mock<ILogger<MonitoringDataRepository>>();
+    private async Task ClearData()
+    {
+        string? connectionString = config.GetConnectionString(DapperContext.ConnectionString);
+        using IDbConnection connection = new NpgsqlConnection(connectionString);
 
-            var repository = new MonitoringDataRepository(context, loggerMock.Object);
-
-            var data = new MonitoringData
-            {
-                Id = Guid.NewGuid().ToString(),
-                Version = "1.2.3",
-                NodeName = "Name",
-                OperatingSystem = "Android",
-                CreatedDate = DateTime.UtcNow,
-                UpdatedDate = DateTime.UtcNow,
-            };
-
-            // Act
-            await repository.CreateAsync(data);
-
-            MonitoringData? result = await repository.GetByIdAsync(data.Id);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.NotNull(result?.Id);
-            Assert.NotNull(result?.NodeName);
-            Assert.NotNull(result?.Version);
-            Assert.NotNull(result?.OperatingSystem);
-            Assert.NotNull(result?.CreatedDate);
-            Assert.NotNull(result?.UpdatedDate);
-            Assert.Equal(data.Id, result?.Id);
-            Assert.Equal(data.Version, result?.Version);
-            Assert.Equal(data.NodeName, result?.NodeName);
-            Assert.Equal(data.OperatingSystem, result?.OperatingSystem);
-            Assert.Equal(data.CreatedDate?.ToString("u"), result?.CreatedDate?.ToString("u"));
-            Assert.Equal(data.UpdatedDate?.ToString("u"), result?.UpdatedDate?.ToString("u"));
-        }
-
-        /// <summary>
-        /// Проверка успешного обновления мониторинговых данных.
-        /// </summary>
-        /// <returns>Задача.</returns>
-        [Fact]
-        public async Task UpdateMonitoringData_Success_addding()
-        {
-            await ClearData();
-
-            // Arrange
-            var context = new DapperContext(config);
-            var loggerMock = new Mock<ILogger<MonitoringDataRepository>>();
-
-            var repository = new MonitoringDataRepository(context, loggerMock.Object);
-
-            var data = new MonitoringData
-            {
-                Id = Guid.NewGuid().ToString(),
-                Version = "1.2.3",
-                NodeName = "Name",
-                OperatingSystem = "Android",
-                CreatedDate = DateTime.UtcNow,
-                UpdatedDate = DateTime.UtcNow,
-            };
-
-            // Act
-            await repository.CreateAsync(data);
-
-            data.NodeName = "Node name";
-            data.Version = "1.1.1";
-            data.OperatingSystem = "Ios";
-            data.CreatedDate = DateTime.UtcNow;
-            data.UpdatedDate = DateTime.UtcNow;
-
-            await repository.UpdateAsync(data);
-
-            MonitoringData? result = await repository.GetByIdAsync(data.Id);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.NotNull(result?.Id);
-            Assert.NotNull(result?.NodeName);
-            Assert.NotNull(result?.Version);
-            Assert.NotNull(result?.OperatingSystem);
-            Assert.NotNull(result?.CreatedDate);
-            Assert.NotNull(result?.UpdatedDate);
-            Assert.Equal(data.Id, result?.Id);
-            Assert.Equal(data.NodeName, result?.NodeName);
-            Assert.Equal(data.Version, result?.Version);
-            Assert.Equal(data.OperatingSystem, result?.OperatingSystem);
-            Assert.Equal(data.CreatedDate?.ToString("u"), result?.CreatedDate?.ToString("u"));
-            Assert.Equal(data.UpdatedDate?.ToString("u"), result?.UpdatedDate?.ToString("u"));
-        }
-
-        private async Task ClearData()
-        {
-            string? connectionString = config.GetConnectionString(DapperContext.ConnectionString);
-            using IDbConnection connection = new NpgsqlConnection(connectionString);
-
-            await connection.ExecuteAsync("DELETE FROM monitoring_data;");
-            await connection.ExecuteAsync("DELETE FROM node_events;");
-        }
+        await connection.ExecuteAsync("DELETE FROM monitoring_data;");
+        await connection.ExecuteAsync("DELETE FROM node_events;");
     }
 }
---
+++ Infotecs.Mobile.Monitoring.Tests/WrongMonitoringData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Infotecs.Mobile.Monitoring.Core.Models;
+
+namespace Infotecs.Mobile.Monitoring.Tests;
+
+/// <summary>
+/// Класс, который используется для проверки пполучения исключения при попытке сохранения невалидных данных.
+/// </summary>
+public class WrongMonitoringData : IEnumerable<object[]>
+{
+    /// <inheritdoc />
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { new MonitoringData() };
+        yield return new object[] { new MonitoringData
+        {
+            Id = null,
+            Version = "3.2.1",
+            NodeName = "Name",
+            OperatingSystem = "Ios",
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+        }};
+        yield return new object[] { new MonitoringData
+        {
+            Id = Guid.NewGuid().ToString(),
+            Version = null,
+            NodeName = "Name",
+            OperatingSystem = "Ios",
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+        }};
+        yield return new object[] { new MonitoringData
+        {
+            Id = Guid.NewGuid().ToString(),
+            Version = "3.2.1",
+            NodeName = null,
+            OperatingSystem = "Ios",
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+        }};
+        yield return new object[] { new MonitoringData
+        {
+            Id = Guid.NewGuid().ToString(),
+            Version = "3.2.1",
+            NodeName = "Name",
+            OperatingSystem = null,
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+        }};
+        yield return new object[] { new MonitoringData
+        {
+            Id = Guid.NewGuid().ToString(),
+            Version = "3.2.1",
+            NodeName = "Name",
+            OperatingSystem = "Ios",
+            CreatedDate = null,
+            UpdatedDate = DateTime.UtcNow,
+        }};
+        yield return new object[] { new MonitoringData
+        {
+            Id = Guid.NewGuid().ToString(),
+            Version = "3.2.1",
+            NodeName = "Name",
+            OperatingSystem = "Ios",
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = null,
+        }};
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
